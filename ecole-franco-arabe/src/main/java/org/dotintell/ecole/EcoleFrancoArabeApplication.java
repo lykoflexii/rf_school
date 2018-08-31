a1f3c9b1d1e00ebca92dc92e52138972293682c4
lykoflexii <kote.proformating@gmail.com>
2018-08-31 10:15:53 +0000

    adding back-end part to the project

---
+++ ecole-franco-arabe/src/main/java/org/dotintell/ecole/EcoleFrancoArabeApplication.java
@@ -0,0 +1,12 @@
+package org.dotintell.ecole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoleFrancoArabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoleFrancoArabeApplication.class, args);
+	}
+}
